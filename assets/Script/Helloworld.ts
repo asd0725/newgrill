a3f526b876aab0fadb4d5dedbc8521391b55a9e9
wangchao02 <wangchao02@huixuanjiasu.com>
2023-08-05 22:10:51 +0800

    no message

--- assets/Script/Helloworld.ts
+++ assets/Script/Helloworld.ts
@@ -6,11 +6,9 @@ export default class Helloworld extends cc.Component {
     @property(cc.Label)
     label: cc.Label = null;
 
-    @property
-    text: string = 'hello';
+
 
     start () {
         // init logic
-        this.label.string = this.text;
     }
 }
